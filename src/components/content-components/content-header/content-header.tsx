6eacd5dba7af871f980f81bc039c18d172d145e2
bircan <fbircankut@icloud.com>
2025-03-03 00:39:43 +0300

    refactor: change name for content-header component

---
+++ src/components/content-components/content-header/content-header.tsx
@@ -0,0 +1,43 @@
+"use client";
+import IconButton from "@/components/icon-button/icon-button";
+import { LuChevronsLeft } from "react-icons/lu";
+import { LuChevronsRight } from "react-icons/lu";
+import { LuMaximize2 } from "react-icons/lu";
+import { LuMessageSquareMore } from "react-icons/lu";
+import { RiTimerLine } from "react-icons/ri";
+import { LuStar } from "react-icons/lu";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { useSidebar } from "@/contexts/sidebar-context";
+import {
+    ContentTeamMemberPictures
+} from "@/components/content-components/content-team-member-pictures/content-team-member-pictures";
+
+const ContentHeader = () => {
+  const { isSidebarExpanded, handleSidebarState } = useSidebar();
+
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex flex-row">
+        <IconButton
+          className="mx-4"
+          onClick={handleSidebarState}
+          icon={isSidebarExpanded ? LuChevronsLeft : LuChevronsRight}
+        />
+        <IconButton className="mx-4" icon={LuMaximize2} />
+      </div>
+
+      <div className="h-full w-full flex justify-end items-center ">
+        <ContentTeamMemberPictures/>
+      </div>
+
+      <div className="flex flex-row">
+        <IconButton className="mx-4" icon={LuMessageSquareMore} />
+        <IconButton className="mx-4" icon={RiTimerLine} />
+        <IconButton className="mx-4" icon={LuStar} />
+        <IconButton className="mx-4" icon={HiOutlineDotsVertical} />
+      </div>
+    </div>
+  );
+};
+
+export { ContentHeader };
