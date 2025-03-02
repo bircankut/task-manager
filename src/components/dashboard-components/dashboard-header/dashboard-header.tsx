dcea594e2cb32f78662eb471b46dabaccf69dd67
bircan <fbircankut@icloud.com>
2025-03-03 00:45:08 +0300

    refactor: change task-header component's name to dashboard-header

--- src/components/dashboard-components/dashboard-header/dashboard-header.tsx
+++ src/components/dashboard-components/dashboard-header/dashboard-header.tsx
@@ -1,41 +1,39 @@
 "use client";
-import IconButton from "@/components/icon-button/icon-button";
-import { LuChevronsLeft } from "react-icons/lu";
-import { LuChevronsRight } from "react-icons/lu";
-import { LuMaximize2 } from "react-icons/lu";
-import { LuMessageSquareMore } from "react-icons/lu";
-import { RiTimerLine } from "react-icons/ri";
-import { LuStar } from "react-icons/lu";
-import { HiOutlineDotsVertical } from "react-icons/hi";
-import { useSidebar } from "@/contexts/sidebar-context";
-import {
-    DashboardTeamMemberPictures
-} from "@/components/dashboard-components/dashboard-team-member-pictures/dashboard-team-member-pictures";
 
-const DashboardHeader = () => {
-  const { isSidebarExpanded, handleSidebarState } = useSidebar();
+import { useState } from "react";
+import { AddTaskPopUpProps } from "@/components/dashboard-components/add-task-pop-up/add-task-pop-up";
+import { createPortal } from "react-dom";
 
+interface HeaderProps {
+  title: string;
+  taskCount: number;
+  status: string;
+}
+
+const DashboardHeader = ({ title, taskCount, status }: HeaderProps) => {
+  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex flex-row">
-        <IconButton
-          className="mx-4"
-          onClick={handleSidebarState}
-          icon={isSidebarExpanded ? LuChevronsLeft : LuChevronsRight}
-        />
-        <IconButton className="mx-4" icon={LuMaximize2} />
-      </div>
-
-      <div className="h-full w-full flex justify-end items-center ">
-        <DashboardTeamMemberPictures/>
-      </div>
-
-      <div className="flex flex-row">
-        <IconButton className="mx-4" icon={LuMessageSquareMore} />
-        <IconButton className="mx-4" icon={RiTimerLine} />
-        <IconButton className="mx-4" icon={LuStar} />
-        <IconButton className="mx-4" icon={HiOutlineDotsVertical} />
-      </div>
+    <div className="flex flex-row items-center justify-between">
+      <h2 className="flex font-bold items-center justify-between">
+        <span className="mr-2 text-stone-600"> {title} </span>
+        <div className="flex justify-center items-center h-5 w-5 rounded-xl font-light text-sm text-indigo-400 bg-indigo-200">
+          {taskCount}
+        </div>
+      </h2>
+      <button
+        onClick={() => setIsPopUpOpen(true)}
+        className="flex justify-center items-center text-stone-600 rounded relative border border-solid border-stone-600 h-6 w-6"
+      >
+        +
+      </button>
+      {isPopUpOpen &&
+        createPortal(
+          <AddTaskPopUpProps
+            status={status}
+            onClose={() => setIsPopUpOpen(false)}
+          />,
+          document.body,
+        )}
     </div>
   );
 };
