0c0c54c6c2cb4bbf45c618dc903c7165eab14f81
bircan <fbircankut@icloud.com>
2025-03-03 00:38:05 +0300

    feat: declare module for color-thief-browser

---
+++ src/types/color-thief-browser.d.ts
@@ -0,0 +1,6 @@
+declare module "color-thief-browser" {
+    export default class ColorThief {
+        getColor(img: HTMLImageElement): [number, number, number];
+        getPalette(img: HTMLImageElement, colorCount?: number): [number, number, number][];
+    }
+}
