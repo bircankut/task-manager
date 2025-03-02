a407498189697896fc8720218131f1bebbcbddda
bircan <fbircankut@icloud.com>
2025-03-03 00:42:39 +0300

    refactor: change dashboard component's name to content

---
+++ src/app/content/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { ContentComponents } from "@/components/content-components";
+import React, { useState } from "react";
+import { sectionComponents } from "@/components/content-components/content-nav/navbar-index";
+
+interface ContentProps {}
+
+export default function Content({}: ContentProps) {
+  const [selectedSection, setSelectedSection] =
+    useState<keyof typeof sectionComponents>("Dashboard");
+
+  const handleSectionClick = (section: keyof typeof sectionComponents) => {
+    setSelectedSection(section);
+  };
+
+  return (
+    <div className="h-screen w-full flex flex-col">
+      <div className="h-2/5 w-full bg-gray-100 pt-3 pb-6 px-8 flex flex-col justify-between">
+        <ContentComponents.Header />
+        <ContentComponents.Banner />
+        <ContentComponents.Nav handleSectionClick={handleSectionClick} selectedSection={selectedSection} />
+      </div>
+      <div className="h-3/5 w-full bg-gray-100 overflow-y-auto pt-4 pb-4">
+        {sectionComponents[selectedSection]()}
+      </div>
+    </div>
+  );
+}
--- src/components/dashboard-components/dashboard/dashboard.tsx
+++ src/components/dashboard-components/dashboard/dashboard.tsx
@@ -11,9 +11,9 @@ import {
   DragEndEvent,
   DragStartEvent,
 } from "@dnd-kit/core";
-import { TaskHeader } from "@/components/task-components/task-header/task-header";
-import Droppable from "@/components/task-components/task-board/droppable";
-import Draggable from "@/components/task-components/task-board/draggable";
+import { DashboardHeader } from "@/components/dashboard-components/dashboard-header/dashboard-header";
+import Droppable from "@/components/dashboard-components/dashboard/droppable";
+import Draggable from "@/components/dashboard-components/dashboard/draggable";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { RiAttachmentLine } from "react-icons/ri";
 import { LuMessagesSquare } from "react-icons/lu";
@@ -21,7 +21,7 @@ import IconButton from "@/components/icon-button/icon-button";
 import { useProject } from "@/contexts/project-context";
 import cns from "classnames";
 import { createPortal } from "react-dom";
-import { EditTask } from "@/components/task-components/edit-task/edit-task";
+import { EditTask } from "@/components/dashboard-components/edit-task/edit-task";
 
 export const COLORS = [
   "bg-indigo-100",
@@ -37,7 +37,7 @@ const sections = [
   { title: "Done", status: "done" },
 ];
 
-const TaskBoard = () => {
+const Dashboard = () => {
   const { currentProject, updateTask, addTask, setCurrentProject } =
     useProject();
   const [activeTaskId, setActiveTaskId] = useState<number | null>(null);
@@ -94,7 +94,7 @@ const TaskBoard = () => {
         {sections.map((section, key) => (
           <div key={key} className="flex flex-col h-full w-full">
             <div className="sticky top-0 bg-gray-100 py-2">
-              <TaskHeader
+              <DashboardHeader
                 key={section.status}
                 title={section.title}
                 taskCount={
@@ -272,4 +272,4 @@ const TaskBoard = () => {
   );
 };
 
-export { TaskBoard };
+export { Dashboard };
