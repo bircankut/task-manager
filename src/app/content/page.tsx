3eaf3b6fc29ca4e49149ea4f1054df5701a73382
bircan <fbircankut@icloud.com>
2025-06-11 15:05:32 +0200

    Refactor: move sidebar to content page from layout

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -2,6 +2,8 @@
 import { ContentComponents } from "@/components/content-components";
 import React, { useState } from "react";
 import { sectionComponents } from "@/components/content-components/content-nav/navbar-index";
+import { Sidebar } from "@/components/sidebar/sidebar";
+import { Profile } from "@/components/profile/profile";
 
 interface ContentProps {}
 
@@ -14,15 +16,22 @@ export default function Content({}: ContentProps) {
   };
 
   return (
-    <div className="h-screen w-full flex flex-col">
-      <div className="h-2/5 w-full bg-gray-100 pt-3 pb-6 px-8 flex flex-col justify-between">
-        <ContentComponents.Header />
-        <ContentComponents.Banner />
-        <ContentComponents.Nav handleSectionClick={handleSectionClick} selectedSection={selectedSection} />
+    <div className="h-screen w-full flex flex-row">
+      <Sidebar />
+      <div>
+        <div className="h-2/5 w-full bg-gray-100 pt-3 pb-6 px-8 flex flex-col justify-between">
+          <ContentComponents.Header />
+          <ContentComponents.Banner />
+          <ContentComponents.Nav
+            handleSectionClick={handleSectionClick}
+            selectedSection={selectedSection}
+          />
+        </div>
+        <div className="h-3/5 w-full bg-gray-100 overflow-y-auto pt-4 pb-4">
+          {sectionComponents[selectedSection]()}
+        </div>
       </div>
-      <div className="h-3/5 w-full bg-gray-100 overflow-y-auto pt-4 pb-4">
-        {sectionComponents[selectedSection]()}
-      </div>
+      <Profile />
     </div>
   );
 }
